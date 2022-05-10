639e1b6bb8131c31ca2cdc2574e3870c613b9c82
naveen7496 <nknaveen379@gmail.com>
2022-05-10 22:56:03 +0530

    taxi project added

--- src/main/scala/Taxi.scala
+++ src/main/scala/Taxi.scala
@@ -23,17 +23,53 @@ object Taxi extends App {
 
   // 3. Fraction of amount paid to toll
   val toll = ss.sql("SELECT SUM(tolls_amount) FROM tbl_taxi")
-
-  total.show()
-  toll.show()
-
+//
+//  total.show()
+//  toll.show()
+//
   val a = total.first().getDouble(0)
-  val b = toll.first().getAs[Double](0)
+//  val b = toll.first().getAs[Double](0)
+//
+//  val toll_fraction = (b / a) * 100
+//  println(toll_fraction)
 
-  val toll_fraction = (b / a) * 100
-  println(a)
-  println(b)
-  println(toll_fraction)
+
+  // 4. Fraction of amount paid to driver tip
+
+  val driver_tip = ss.sql("SELECT SUM(tip_amount) FROM tbl_taxi").first().getDouble(0)
+
+  val tip_fraction = (driver_tip / a) * 100
+//  println(driver_tip)
+//  println(tip_fraction)
+
+  // 5. average trip amount
+
+  val avgTripAmount = ss.sql("SELECT AVG(total_amount) FROM tbl_taxi").first().getDouble(0)
+//  println(avgTripAmount)
+
+
+  // 6. average trip distance
+  val avgDistance = ss.sql("SELECT AVG(trip_distance) FROM tbl_taxi").first().getDouble(0)
+//  println(avgDistance)
+
+  // 7. how many different payment types
+  val paymentTypes = ss.sql("SELECT DISTINCT payment_type FROM tbl_taxi")
+//  paymentTypes.show()
+//  println(paymentTypes.count())
+
+//  ss.sql("SELECT pickup_datetime,payment_type,tip_amount FROM tbl_taxi WHERE payment_type = 4").show()
+
+
+
+// 8.  For each payment type, display the following details:
+//    a. Average fare generated
+//  b. Average tip
+//    c. Average tax – tax is stored in the column, mta_tax
+
+val paymentAnalysis = ss.sql("SELECT payment_type,AVG(fare_amount),AVG(tip_amount),AVG(mta_tax) FROM tbl_taxi GROUP BY payment_type")
+//  paymentAnalysis.show()
+
+
 
 
 }
